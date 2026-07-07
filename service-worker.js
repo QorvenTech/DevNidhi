7d42d99eae2d11769e94981bd83b7fd38c67483d
SharmaHarsh10 <harshuashokafan@gmail.com>
2026-07-07 20:20:21 +0530

    Redesign Kharcha screen

--- service-worker.js
+++ service-worker.js
@@ -4,13 +4,13 @@
  * Firestore manages record-level offline persistence and synchronization.
  */
 
-const CACHE_VERSION = "devnidhi-v16";
+const CACHE_VERSION = "devnidhi-v17";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.json",
   "./firebase-config.js",
-  "./app.js?v=14",
+  "./app.js?v=15",
   "./logo.svg",
   "./icon-192.png",
   "./icon-512.png"
